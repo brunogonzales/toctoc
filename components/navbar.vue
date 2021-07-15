b30cf7c8613b2992d5821eeafd793c19d22fbe77
Bruno Gonzales <holabrunogonzales@gmail.com>
2021-07-15 17:35:10 -0500

    refactoring nav component

---
+++ components/navbar.vue
@@ -0,0 +1,34 @@
+<template>
+		<nav class="absolute bottom-0 left-0 flex justify-between self-end">
+			<nuxt-link class="relative block px-4 py-2" v-for="link in links" :to="link.path">{{link.name}}</nuxt-link>
+		</nav>
+</template>
+<script>
+export default {
+	data(){
+		return {
+			links: [
+				{ name: 'Home', path:"/" },
+				{ name: 'Discover', path:"/discover" },
+				{ name: 'Create', path: "/create" },
+				{ name: 'Inbox', path: "/inbox" },
+				{ name: 'Me', path: "/me" },
+			]
+		}
+	},
+}
+
+</script>
+<style>
+.nuxt-link-exact-active::before {
+	content: "";
+	display: block;
+	height: 10px;
+	width: 10px;
+	background: red;
+	border-radius: 4px;
+	position: absolute;
+	top: 5px;
+	left: 2px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,25 +4,9 @@
 		<main class="flex-grow">
 			video
 		</main>
-		<nav class="absolute bottom-0 left-0 flex justify-between self-end">
-			<nuxt-link class="block px-4 py-2" v-for="link in links" :to="link.path">{{link.name}}</nuxt-link>
-		</nav>
 	</div>
 </template>
 
 <script>
-export default {
-	data(){
-		return {
-			links: [
-				{ name: 'Home', path:"/" },
-				{ name: 'Discover', path:"/discover" },
-				{ name: 'Create', path: "/create" },
-				{ name: 'Inbox', path: "/inbox" },
-				{ name: 'Me', path: "/me" },
-			]
-		}
-	},
-}
-
+export default {}
 </script>
